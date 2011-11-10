0e5b5f8ed3116925be414e3a9e84eb7a59e0a5eb
Squeeks <privacymyass@gmail.com>
2011-11-10 12:00:35 +0000

    Version 0.02

--- lib/App/CPAN/MetaDB.pm
+++ lib/App/CPAN/MetaDB.pm
@@ -6,11 +6,11 @@ App::CPAN::MetaDB - Provide CPAN metadata for cpanminus clients
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
